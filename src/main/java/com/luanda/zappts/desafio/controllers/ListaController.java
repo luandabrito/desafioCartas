fde1b311854b5bdd99082f5639c85a083ae52b4a
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2022-05-04 19:55:12 -0300

    add funcionalidade para resgatar todas as listas

--- src/main/java/com/luanda/zappts/desafio/controllers/ListaController.java
+++ src/main/java/com/luanda/zappts/desafio/controllers/ListaController.java
@@ -5,10 +5,9 @@ import com.luanda.zappts.desafio.service.ListaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/listas")
@@ -31,6 +30,19 @@ public class ListaController {
         }
 
         return response;
+    }
+
+    @GetMapping
+    public List<Lista> pegarTodasListas(){
+        List<Lista> response = null;
+
+        try {
+            logger.info("Iniciando resgate das listas");
+            response =  listaService.pegarTodasListas();
+        } catch (Exception e) {
+            logger.error("Erro ao resgatar listas: {}", e.getMessage());
+        }
+       return response;
     }
 
 }
--- src/main/java/com/luanda/zappts/desafio/service/ListaService.java
+++ src/main/java/com/luanda/zappts/desafio/service/ListaService.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ListaService {
 
@@ -23,4 +25,8 @@ public class ListaService {
         return response;
     }
 
+    public List<Lista> pegarTodasListas() {
+        List<Lista> response = listaRepository.findAll();
+        return response;
+    }
 }
